c2fe3f0d33f26523c73809f6333939aa9f59b70a
gauravnepal32 <gauravnepal3@gmail.com>
2023-09-23 17:42:09 +0545

    Inital Commit

---
+++ assests/script.js
@@ -0,0 +1,10 @@
+// const handleNavbar = () => {
+//     var navbutton = document.getElementById("navbarButton");
+//     if (navbutton.contains("openNav")) {
+//         navbutton.classList.remove("openNav");
+//     } else {
+//         navbutton.classList.add("openNav");
+//     }
+// };
+
+
